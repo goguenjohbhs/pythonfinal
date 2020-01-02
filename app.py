7fcec2de1acc032b6a6760e8d309d5216059685b
john <john@Johns-MacBook-Pro.local>
2020-01-02 15:21:22 -0500

    added top level cli (facts/trivia/quit) and added quit feature

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+def main ():
+
+    while 1:
+        cmd = input ("(f/facts) (t/trivia) (q/quit/stop): ")
+        cmd = cmd.lower()
+
+        if  (cmd=="") or (cmd=="q") or (cmd=="quit") or (cmd=="stop"):
+            break
+
+
 if __name__ == '__main__':
     print("Welcome to Sports App")
+    main()
