fc69ec95dfce9fcdace3c928528c9a678639dc9d
rgoguen <rgoguen@cisco.com>
2020-01-16 19:57:03 -0500

    added comments

--- app.py
+++ app.py
@@ -2,19 +2,35 @@
 import facts
 import trivia
 
+# Main:
+#    - High level CLI to control whether we quit / call facts / call triva
+
 def main ():
-#def of main means run whatever is below a while statment is a loop which means true and runs forever
+
+    # Loop forever, because 1 is true
     while 1:
+
+        # Get input from user, assign what they type to the variable "cmd"
         cmd = input ("(f/facts) (t/trivia) (q/quit/stop): ")
+
+        # convert all input to lower, to help minimize what we need to test for
         cmd = cmd.lower()
 
+        # complese if ... elfi to determine what the user entered up above
         if  (cmd=="") or (cmd=="q") or (cmd=="quit") or (cmd=="stop"):
+            # user choose to quit, so we break out of while loop, leaving the app
             break
         elif (cmd=="f") or (cmd=="facts"):
+            # user choose facts, so call the entrypoint to the facts package
             facts.run_facts()
         elif (cmd=="t") or (cmd=="trivia"):
+            # user choose trivia, so call the entrypoint to the triviapackage
             trivia.run_trivia()
+
+# Python entrypoint starts at the following code
 
 if __name__ == '__main__':
     print("Welcome to Sports App")
+
+    # Call the main Command Line Interface (CLI) for our app
     main()
--- facts.py
+++ facts.py
@@ -5,13 +5,16 @@ from sportsreference.nfl.schedule import Schedule
 from sportsreference.nfl.roster import Roster
 
 def list_team_names(my_year):
-# if you give an unknown year or team it will go to the except block which prints out no data for that year then return to the main page in this code
+
+    # if you give an unknown year or team it will go to the except block which prints out no data for that year
+    # then return to the main page in this code
     try:
         allTeams = Teams(year=my_year)
     except:
         print ("No data for Year %s" % my_year)
         return
 
+    # walk through each team and print out just the long name and the abbreviated name
     for team in allTeams:
         print("%-22s %-3s" % (team.name, team.abbreviation))
 
@@ -22,8 +25,13 @@ def display_teams(my_year):
         print ("No data for Year %s" % my_year)
         return
 
+    # Let's sort the list of teams by wins in ascending order
+    # The build-in python sorted function takes two parameters
+    #    - the list "allTeams" which was return from call to Teams above
+    #    - the key, which is a small function (lambda) which just returns what we are sorting on "team.wins"
     sortedTeams = sorted(allTeams, key=lambda team: team.wins)
 
+    # walk through each team and print out more information
     for team in sortedTeams:
         print("%-20s %-11s Won: %2d Lost: %2d pts+: %-3d pts-: %-3d ptdiff: %-4d" %
         (team.name,
@@ -34,31 +42,37 @@ def display_teams(my_year):
          team.points_against,
          team.points_difference))
 
-
-
 def display_results(my_year):
+
+    # stores the abbreviated names in a dictionary which converts abbr names to full name
     abbrevName2Name = {}
 
-    # games returned have opponents listed w/abbrev name
-    # walk through all the teams, saving a dictionary of abbrev name to actual team name
     allTeams = Teams(year=my_year)
+
+    # walk through all the teams, saving a dictionary of abbrev name to actual team name
     for team in allTeams:
         abbrevName2Name[team.abbreviation] = team.name
 
+    # Loop forever
     while 1:
         name = input("Team Name (l/list/q/quit/stop): ")
+
+        # this time we convert to UPPER, because we need abbreviated name in UpperCase
         name = name.upper()
 
         if (name== "") or (name== "Q") or (name== "QUIT") or (name== "STOP"):
             break
+
         if (name== "L") or (name=="LIST"):
             list_team_names(my_year)
+            # "continue" goes back up to run the while loop again
             continue
 
         try:
             allgames = Schedule(name, year=my_year)
         except:
             print("This is an unknown team or an unavaliable year")
+            # "continue" goes back up to run the while loop again
             continue
 
         teamName = abbrevName2Name[name]
@@ -66,13 +80,20 @@ def display_results(my_year):
         lost = 0
         tie = 0
 
-
+        # Walk through all the the games, keeping track of
+        #    - number of wins
+        #    - number of losses
+        #    - number of ties
+        # And print out each games result to the user
+        
         for game in allgames:
+            # if points_allowed is equal "None", then there are no more valid games in this list
             if game.points_allowed is None:
                 break
 
             oppAbbr = game.opponent_abbr.upper()
             oppName = abbrevName2Name[oppAbbr]
+
             if game.result is None:
                 result = "Not Played"
             else:
@@ -93,7 +114,7 @@ def display_results(my_year):
                 game.points_allowed,
                 game.type))
 
-            print("Record: Wins: %d Loss: %d Ties: %d" % (won,lost,tie))
+        print("Record: Wins: %d Loss: %d Ties: %d" % (won,lost,tie))
 
 
 def display_players(my_year):
@@ -125,20 +146,29 @@ def display_players(my_year):
 
 
 
+# Main function for the facts CLI
+#     - Basically run another while loop for this module commands
 
 def run_facts():
     year=""
 
+    # Loop forever, because 1 is true
     while 1:
+
+        # Get input from user, assign what they type to the variable "cmd"
         cmd = input ("(l/list) (t/team) (p/player) (r/results) (y/year) (q/quit/stop): ")
+
+        # convert all input to lower, to help minimize what we need to test for
         cmd = cmd.lower()
 
+        # complese if ... elfi to determine what the user entered up above
         if  (cmd=="") or (cmd=="q") or (cmd=="quit") or (cmd=="stop"):
+            # user wants to quit, break, then return from function to main cli
             break
         elif (cmd=="l") or (cmd=="list"):
              list_team_names(year)
-        elif (cmd=="t") or (cmd=="team"):
-             display_teams(year)
+        elif (cmd=="t") or (cmd=="team"): 
+            display_teams(year)
         elif (cmd=="r") or (cmd=="results"):
              display_results(year)
         elif (cmd=="y") or (cmd=="year"):
@@ -146,7 +176,6 @@ def run_facts():
         elif (cmd=="p") or (cmd=="player"):
              display_players(year)
 
-
-
+# Python entrypoint if we just want to run facts.py alone
 if __name__ == '__main__':
      run_facts()
